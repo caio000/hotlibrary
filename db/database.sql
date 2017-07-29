2114056dff6495e86b29a988ac42e8b21b81993e
Caio de Freitas <caiof.adriano@hotmail.com>
2017-07-29 11:45:50 -0300

    Database directory

---
+++ db/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE "HOTLIBRARY";
+
+CREATE TABLE "User"(
+  id        SERIAL        NOT NULL,
+  name      VARCHAR(100)  NOT NULL,
+  email     VARCHAR(100)  NOT NULL  UNIQUE,
+  password  VARCHAR(128)  NOT NULL,
+  isActive  BOOLEAN       NOT NULL  DEFAULT TRUE,
+  isAdmin   BOOLEAN       NOT NULL  DEFAULT FALSE,
+  PRIMARY KEY (id)
+);
