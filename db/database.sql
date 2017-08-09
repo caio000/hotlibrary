dcf348b3fe668920373eeb715bd3a1bd6371ebe4
Caio de Freitas <caiof.adriano@hotmail.com>
2017-08-09 11:32:27 -0300

    Create log table

--- db/database.sql
+++ db/database.sql
@@ -21,3 +21,15 @@ CREATE TABLE "User"(
 );
 -- Usuário admin senha 'teste123'
 INSERT INTO "User" ("name","email","password","level") VALUES('admin','admin@hotlibrary.com','535f56e6447ea0fcf3ef1bf5397066d037e9ebb7fd141068e8de9a23ece8eb6e7acf46d0e6bbf17edf2ebe6c80405991be53366138e835c3153019f164340619',1);
+
+CREATE TABLE "Log" (
+  "id"      SERIAL        NOT NULL,
+  "ip"      CIDR          NOT NULL,
+  "date"    DATE          NOT NULL,
+  "time"    TIME          NOT NULL,
+  "message" VARCHAR(255)  NOT NULL,
+  "idUser"  INTEGER       NOT NULL,
+  PRIMARY KEY ("id"),
+  CONSTRAINT fk_log_user
+    FOREIGN KEY ("idUser") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
