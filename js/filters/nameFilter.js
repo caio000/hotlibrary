6cd25302c1ca2f3c040dcad32c96e254139a5b7e
Caio de Freitas <caiof.adriano@hotmail.com>
2017-10-26 11:32:42 -0200

    created name filter. For every word get the first letter and convert to uppercase

---
+++ js/filters/nameFilter.js
@@ -0,0 +1,13 @@
+hotlibrary.filter('name', function() {
+  return function (input) {
+    input = input.toLowerCase();
+    var name = input.split(" ");
+
+    input = name.map(function(subName){
+      var subName = subName.charAt(0).toUpperCase() + subName.substring(1).toLowerCase();
+      return subName;
+    });
+
+    return input.join(" ");
+  };
+})
